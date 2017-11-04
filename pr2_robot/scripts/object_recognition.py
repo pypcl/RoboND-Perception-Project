3ebfc1bb8f0a8af84749a2a40e58e35026d49aca
skreynolds <shane.k.reynolds@gmail.com>
2017-11-04 17:52:02 +0930

    Added Euclidean clustering using DBSCAN - also added another dimension to the PassThrough filter - the edges of the box were making it into the frame, which would have meant that the system would classify them as an object. This would have caused issues in the future. The additional dimension is along the y-axis.

--- pr2_robot/scripts/object_recognition.py
+++ pr2_robot/scripts/object_recognition.py
@@ -74,21 +74,37 @@ def pcl_callback(pcl_msg):
     # Call the filter function to obtain the resultant downsampled point cloud
     cloud_filtered = vox.filter()
 
+
 ##############################################################################
     # TODO: PassThrough Filter
 ##############################################################################
     # Create a PassThrough filter object.
-    passthrough = cloud_filtered.make_passthrough_filter()
+    passthrough_z = cloud_filtered.make_passthrough_filter()
 
-    # Assign axis and range to the passthrough filter object.
+    # ZAXIS Assign axis and range to the passthrough filter object.
     filter_axis = 'z'
-    passthrough.set_filter_field_name(filter_axis)
-    axis_min = 0.75
-    axis_max = 1.1
-    passthrough.set_filter_limits(axis_min, axis_max)
+    passthrough_z.set_filter_field_name(filter_axis)
+    axis_min = 0.60
+    axis_max = 1.8
+    passthrough_z.set_filter_limits(axis_min, axis_max)
 
-    # Finally use the filter function to obtain the resultant point cloud.
-    cloud_filtered = passthrough.filter()
+    # Use the filter function to obtain the filtered z-axis.
+    cloud_filtered = passthrough_z.filter()
+
+    # Create PassThrough filter object
+    passthrough_y = cloud_filtered.make_passthrough_filter()
+
+    # YAXIS Assign axis and range to the passthrough filter object
+    filter_axis = 'y'
+    passthrough_y.set_filter_field_name(filter_axis)
+    axis_min = -0.5
+    axis_max = 0.5
+    passthrough_y.set_filter_limits(axis_min, axis_max)
+
+    # Use the filter function to obtain the filtered y-axis
+    cloud_filtered = passthrough_y.filter()
+
+
 ##############################################################################
     # TODO: RANSAC Plane Segmentation
 ##############################################################################
@@ -102,11 +118,13 @@ def pcl_callback(pcl_msg):
     # Max distance for a point to be considered fitting the model
     # Experiment with different values for max_distance
     # for segmenting the table
-    max_distance = 0.02
+    max_distance = 0.01
     seg.set_distance_threshold(max_distance)
 
     # Call the segment function to obtain set of inlier indices and model coefficients
     inliers, coefficients = seg.segment()
+
+
 ##############################################################################
     # TODO: Extract inliers and outliers
 ##############################################################################
@@ -116,18 +134,69 @@ def pcl_callback(pcl_msg):
     # Extract outliers
     cloud_objects = cloud_filtered.extract(inliers, negative=True)
 
+
+##############################################################################
     # TODO: Euclidean Clustering
+##############################################################################
+    white_cloud = XYZRGB_to_XYZ(cloud_objects)
+    tree = white_cloud.make_kdtree()
 
+    # Create a cluster extraction object
+    ec = white_cloud.make_EuclideanClusterExtraction()
+
+    # Set tolerances for distance threshold
+    # as well as minimum and maximum cluster size (in points)
+    # NOTE: These are poor choices of clustering parameters
+    # Your task is to experiment and find values that work for segmenting objects.
+    ec.set_ClusterTolerance(0.05)
+    ec.set_MinClusterSize(50)
+    ec.set_MaxClusterSize(3000)
+
+    # Search the k-d tree for clusters
+    ec.set_SearchMethod(tree)
+
+    # Extract indices for each of the discovered clusters
+    cluster_indices = ec.Extract()
+
+
+##############################################################################
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+##############################################################################
+    #Assign a color corresponding to each segmented object in scene
+    cluster_color = get_color_list(len(cluster_indices))
 
+    color_cluster_point_list = []
+
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            color_cluster_point_list.append([white_cloud[indice][0],
+                                            white_cloud[indice][1],
+                                            white_cloud[indice][2],
+                                            rgb_to_float(cluster_color[j])])
+
+    #Create new cloud containing all clusters, each with unique color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
+
+
+##############################################################################
     # TODO: Convert PCL data to ROS messages
+##############################################################################
     ros_cloud_table = pcl_to_ros(cloud_table)
     ros_cloud_objects = pcl_to_ros(cloud_objects)
+    ros_cluster_cloud = pcl_to_ros(cluster_cloud)
 
+
+##############################################################################
     # TODO: Publish ROS messages
+##############################################################################
     pcl_table_pub.publish(ros_cloud_table)
     pcl_objects_pub.publish(ros_cloud_objects)
+    pcl_cluster_pub.publish(ros_cluster_cloud)
 
+''' THIS NEEDS TO BE IMPLEMENTED AT SOME STAGE - THE REASON FOR COMMENTING
+    IT OUT IS BECAUSE THE WAIT PICK AND PLACE SERVICE STOPS THE CONTINUOUS
+    AQUISTION OF POINT CLOUDS
 
 # Exercise-3 TODOs:
 
@@ -142,9 +211,6 @@ def pcl_callback(pcl_msg):
         # Publish a label into RViz
 
         # Add the detected object to the list of detected objects.
-
-    ###CHANGE THIS###
-    detected_objects_list = None
 
     # Publish the list of detected objects
 
@@ -192,7 +258,7 @@ def pr2_mover(object_list):
             print "Service call failed: %s"%e
 
     # TODO: Output your request parameters into output yaml file
-
+'''
 
 
 if __name__ == '__main__':
@@ -206,7 +272,7 @@ if __name__ == '__main__':
     # TODO: Create Publishers
     pcl_table_pub = rospy.Publisher('/pcl_table', PointCloud2, queue_size=1)
     pcl_objects_pub = rospy.Publisher('/pcl_objects', PointCloud2, queue_size=1)
-    #pcl_cluster_pub =
+    pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
 
     # TODO: Load Model From disk
 
